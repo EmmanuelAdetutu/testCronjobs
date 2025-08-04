c482205df4ec211d9d03af2714d362183029dac4
emmatutuvd <emmanuel.adetutu@vanoord.com>
2025-08-04 08:41:54 +0200

    codefortest

---
+++ MyTimerFunction/__init__.py
@@ -0,0 +1,8 @@
+import datetime
+import logging
+import azure.functions as func
+
+def main(mytimer: func.TimerRequest) -> None:
+    utc_timestamp = datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
+    logging.info(f"Timer trigger function ran at {utc_timestamp}")
+    print("This is a scheduled job running from GitHub deployment!")
